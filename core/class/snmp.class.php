e38ad79f6ec80a3f0aaeee72f15b678132bd7c88
Halbout Michael <michaelhalbout@gmail.com>
2020-05-18 08:01:40 +0200

    Create snmp.class.php

---
+++ core/class/snmp.class.php
@@ -0,0 +1,11 @@
+<?php
+require_once dirname(__FILE__) . '/../../../../core/php/core.inc.php';
+class snmp extends eqLogic {
+
+}
+class snmpCmd extends cmd {
+
+	public function execute($_options = null) {	
+	}
+}
+?>
